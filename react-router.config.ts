b2d3a4fe3a48260d241218ac8cf3529aa6938691
wen.you <wen@local>
2026-07-01 15:19:56 +0800

    update ssr setting

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
+  ssr: process.env.NODE_ENV !== "production",
   basename: "/stockpile/",
 } satisfies Config;
